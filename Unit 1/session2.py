81130d7b1da542e478075f3a930236147934406c
Najia Jahan <najia.jahan51@myhunter.cuny.edu>
2025-06-05 23:57:17 -0400

    Problem Set 2 Session 1 complete Session 2 started

---
+++ Unit 1/session2.py
@@ -0,0 +1,31 @@
+# Session 2 PROBLEM SET 1
+# Problem 1
+def print_list(lst):
+    for pokemon in lst:
+        print(pokemon)
+
+lst = ["squirtle", "gengar", "charizard", "pikachu"]
+print_list(lst)
+print("\n")
+
+# Problem 2
+def doubled(lst):
+    #for each number in the lst
+    for num in lst:
+        #multiply by 2 and print
+        print(num * 2)
+    
+lst = [1,2,3]
+doubled(lst)
+print("\n")
+
+# Problem 3
+def doubled(lst):
+    #for each number in the lst
+    for num in lst:
+        #multiply by 2 and print
+        print(num * 2)
+    
+lst = [1,2,3]
+doubled(lst)
+print("\n")
