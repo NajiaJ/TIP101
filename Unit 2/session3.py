8546541ae8563c99a6ef6903bc8fa17c0d246822
Najia Jahan <najia.jahan51@myhunter.cuny.edu>
2025-06-08 00:40:35 -0400

    problem 2 done

--- Unit 2/session3.py
+++ Unit 2/session3.py
@@ -15,3 +15,16 @@ def is_subsequence(lst, sequence):
 lst = [5, 1, 22, 25, 6, -1, 8, 10]
 sequence = [1, 6, -1, 10]
 print(is_subsequence(lst, sequence))
+print('\n')
+
+# Problem 2
+def create_dictionary(keys, values):
+    d = {} #initialize dictionary
+    for key in range(len(keys)): #can use zip(keys,values)
+        if key < len(values): 
+            d[keys[key]] = values[key]
+    return d
+
+keys = ["peanut", "dragon", "star", "pop", "space"]
+values = ["butter", "fly", "fish", "corn", "ship"]
+print(create_dictionary(keys, values))
