90aa4f81a2d7132bc8af13268e81f4a429d75188
Najia Jahan <najia.jahan51@myhunter.cuny.edu>
2025-06-09 18:17:57 -0400

    Prblem 3-4

--- Unit 2/session3.py
+++ Unit 2/session3.py
@@ -28,3 +28,50 @@ def create_dictionary(keys, values):
 keys = ["peanut", "dragon", "star", "pop", "space"]
 values = ["butter", "fly", "fish", "corn", "ship"]
 print(create_dictionary(keys, values))
+print('\n')
+
+# Problem 3
+def print_pair(dictionary, target):
+    if target in dictionary:
+        print("Key:", target)
+        print("Value:", dictionary[target])  # or dictionary.get(target)
+    else:
+        print("That pair does not exist!")
+
+dictionary = {"spongebob": "squarepants", "patrick": "star", "squidward": "tentacles"}
+print_pair(dictionary, "patrick")
+print_pair(dictionary, "plankton")
+print_pair(dictionary, "spongebob")
+print('\n')
+
+# Problem 4
+def keys_v_values(dictionary):
+    #set up sum key and val variables
+    sum_key = 0
+    sum_val = 0
+    #go through the dictionary
+    for key,value in dictionary.items():
+        #add up all keys
+        sum_key += key
+        #add up all values
+        sum_val += value
+        #if sum of keys > values
+        if sum_key > sum_val:
+            #print keys
+            return 'keys'
+        #elif 
+        elif sum_val > sum_key:
+            #print values
+            return 'values'
+        #elif ==
+        elif sum_key == sum_val:
+            #print balanaced
+            return 'balanced'
+
+dictionary1 = {1:10, 2:20, 3:30, 4:40, 5:50, 6:60}
+greater_sum = keys_v_values(dictionary1)
+print(greater_sum)
+
+dictionary2 = {100:10, 200:20, 300:30, 400:40, 500:50, 600:60}
+greater_sum = keys_v_values(dictionary2)
+print(greater_sum)
