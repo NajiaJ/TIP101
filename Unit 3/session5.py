9769e72f6e269df786e3e6777b7b3412547125ce
Najia Jahan <najia.jahan51@myhunter.cuny.edu>
2025-06-16 00:35:08 -0400

    finished problem set 2

--- Unit 3/session5.py
+++ Unit 3/session5.py
@@ -117,3 +117,99 @@ print(dist2)
 words2 = ["code", "path", "code", "contribute",  "practice"]
 dist3 = min_distance(words2, "code", "practice")
 print(dist3)
+print('\n')
+
+# PROBLEM SET 2
+
+# Problem 1
+def match_made(dictionary):
+	for key, value in dictionary.items():
+		print( f"{key} and {value} are a perfect match.")
+
+dictionary = {"Peanut Butter":"Jelly", "Spongebob":"Patrick", "Ash":"Pikachu"}
+match_made(dictionary)
+print('\n')
+
+# Problem 2
+def remove_char(s, n):
+    new_s = ""
+    for index, char in enumerate(s):
+        if index is not n:
+            new_s += char
+    
+    return new_s
+
+s = "typpo"
+fixed_s = remove_char(s, 2)
+print(fixed_s)
+print('\n')
+
+# Problem 3
+def vowel_count(s):
+    vowel_total = 0
+
+    for char in s:
+        if char == 'A' or char == 'a' or char == 'E' or char == 'e' or char == 'I' or char == 'i' or char == 'O' or char == 'o' or char == 'U' or char == 'u':
+            vowel_total += 1
+    
+    return vowel_total
+
+my_str = "hello world"
+my_str2 = "aAaAaAaAAA"
+my_str3 = "ths strng s mssng vwls"
+
+count1 = vowel_count(my_str)
+print(count1)
+count2 = vowel_count(my_str2)
+print(count2)
+count3 = vowel_count(my_str3)
+print(count3)
+print('\n')
+
+# Problem 4
+def reverse_sentence(sentence):
+    spilt = sentence.split()
+    new_sentence = ""
+
+    for i in range(len(spilt) - 1, -1, -1):
+        new_sentence += spilt[i] + " "
+
+    return new_sentence
+
+sentence = "I solemnly swear I am up to no good"
+print(reverse_sentence(sentence))
+print('\n')
+
+# Problem 5
+def compress_string(my_str):
+    letter_dict = {}  # repurposed as a tracker, not a true dictionary
+    new_str = ""
+
+    if not my_str:
+        return my_str
+
+    prev_letter = my_str[0]
+    count = 1
+
+    for letter in my_str[1:]:
+        if letter == prev_letter:
+            count += 1
+        else:
+            letter_dict[prev_letter] = count  # save run
+            new_str += prev_letter + str(count)
+            prev_letter = letter
+            count = 1
+
+    # Add the last run
+    letter_dict[prev_letter] = count
+    new_str += prev_letter + str(count)
+
+    return new_str if len(new_str) < len(my_str) else my_str
+
+my_str = "aaaaabbcccd"
+compressed_Str = compress_string(my_str)
+print(compressed_Str)
+
+my_str2 = "abcde"
+compressed_Str2 = compress_string(my_str2)
+print(compressed_Str2)
